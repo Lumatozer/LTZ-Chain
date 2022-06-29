1b80eaeebd2a85950b84e82b5880017892746fec
Aarav <pl46u3.ducklinz@gmail.com>
2022-06-29 11:36:48 +0530

    hash test updated

--- hash_test.py
+++ hash_test.py
@@ -12,8 +12,8 @@ test_time=10
 hashes=0
 print(make_warning(f"Test Started. Please wait for {test_time} seconds before the test results are shown."))
 end=time()+test_time
-test=b'a'*500
+test='{"checksum":"edbadfbc0339425e3bc08a76c55901b188621c67ca332e65b7b44063f022a467","nonce":9999999'
 while time()<=end:
-    sha256(test)
+    sha256(test.encode()).hexdigest()
     hashes+=1
 print(arrow_msg_gen("Mining Test",f"Your hashrate was {(hashes/test_time)/1000}k hashes per second!"))
